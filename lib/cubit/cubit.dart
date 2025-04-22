fa303b40dd25af49a10ba6dab210b1b9e30d0bb0
moustafaibrahim10 <mustafa444.ibrahim555@gmail.com>
2025-04-22 22:43:00 +0200

    create&Insert database

--- lib/cubit/cubit.dart
+++ lib/cubit/cubit.dart
@@ -1,27 +1,75 @@
 import 'package:flutter/animation.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:sqflite/sqflite.dart';
 import 'package:todo_dsc/cubit/states.dart';
 import 'package:todo_dsc/modules/archive_tasks/archive_tasks.dart';
 import 'package:todo_dsc/modules/done_tasks/done_tasks.dart';
 import 'package:todo_dsc/modules/new_tasks/new_tasks.dart';
 
-class TodoCubit extends Cubit<TodoStates>
-{
-  TodoCubit():super(InitialState());
-  static TodoCubit get(context)=>BlocProvider.of(context);
-  int currentIndex=0;
-  void changeBottomIndex(index)
-  {
-    currentIndex=index;
+class TodoCubit extends Cubit<TodoStates> {
+  TodoCubit() : super(InitialState());
+
+  static TodoCubit get(context) => BlocProvider.of(context);
+  int currentIndex = 0;
+
+  void changeBottomIndex(index) {
+    currentIndex = index;
     emit(ChangeBottomIndexState());
   }
 
-  List<Widget> screens=
-  [
+  List<Widget> screens = [
     NewTasks(), //0
-    DoneTasks(),//1
+    DoneTasks(), //1
     ArchiveTasks(), //2
   ];
 
+  Future<String> getName() async {
+    String name = "moustafa";
+    print(name);
+    return name;
+  }
+
+  Database? database;
+
+  /*
+1-create Database
+2-create tables
+3-open Database
+4-insert to database
+5-get from database
+6-update in database
+7-delete from database
+ */
+  void createDatabase() async {
+    database = await openDatabase(
+      'todo.db',
+      version: 1,
+      onCreate: (database, version) async {
+        await database.execute(
+          'CREATE TABLE tasks (id INTEGER PRIMARY KEY,title TEXT,date Text , time TEXT , status TEXT) ',
+        );
+        print('Database created successfully');
+      },
+      onOpen: (database) {
+        print("Database opened successfully");
+      },
+    );
+  }
+
+  void insertToDatabase(
+      {
+        required String title,
+        required String date,
+        required String time,
+      }) async {
+    await database?.transaction((txn) async {
+      await txn.rawInsert(
+        "INSERT INTO tasks (title,date,time,status) VALUES('$title','$date','$time','new')",
+      );
+      print("data inserted");
+    });
+  }
+
+  //textFormFIeld
 }
--- lib/layout/layout_screen.dart
+++ lib/layout/layout_screen.dart
@@ -9,36 +9,51 @@ class LayoutScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context)=>TodoCubit(),
-      child:BlocConsumer<TodoCubit,TodoStates>(
-          listener: (context,state) {},
-          builder: (context,state)
-          {
-            var cubit=TodoCubit.get(context);
-            return Scaffold(
-              body: cubit.screens[cubit.currentIndex],
-              bottomNavigationBar: BottomNavigationBar(
-                selectedFontSize: 16,
-                currentIndex: cubit.currentIndex,
-                onTap: (index)
-                {
-                  cubit.changeBottomIndex(index);
-                },
-                items: [
-                  BottomNavigationBarItem(icon: Icon(Icons.menu), label: 'New Tasks'),
-                  BottomNavigationBarItem(
-                    icon: Icon(Icons.check_circle_outline),
-                    label: 'Done Tasks',
-                  ),
-                  BottomNavigationBarItem(
-                    icon: Icon(Icons.archive_outlined),
-                    label: 'Archive Tasks',
-                  ),
-                ],
-              ),
-            );
-          },
-    )
+      create: (context) => TodoCubit(),
+      child: BlocConsumer<TodoCubit, TodoStates>(
+        listener: (context, state) {},
+        builder: (context, state) {
+          var cubit = TodoCubit.get(context);
+          return Scaffold(
+            floatingActionButton: FloatingActionButton(
+              backgroundColor: Colors.teal,
+                child: Icon(Icons.add,color: Colors.white,),
+                onPressed: () async
+               {
+                 cubit.insertToDatabase(title: 'Go to gym',time: '12:00',date: '4/22/2025');
+               }
+                ),
+            body: cubit.screens[cubit.currentIndex],
+            bottomNavigationBar: BottomNavigationBar(
+              selectedFontSize: 16,
+              currentIndex: cubit.currentIndex,
+              onTap: (index) {
+                cubit.changeBottomIndex(index);
+              },
+              items: [
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.menu),
+                  label: 'New Tasks',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.check_circle_outline),
+                  label: 'Done Tasks',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.archive_outlined),
+                  label: 'Archive Tasks',
+                ),
+              ],
+            ),
+          );
+        },
+      ),
     );
+  }
+
+
+  Future<String> getName() async
+  {
+     return  "Moustafa";
   }
 }
