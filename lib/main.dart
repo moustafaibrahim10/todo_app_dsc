31b876cb675bf200189a807cce3d0922b03bf3c9
moustafaibrahim10 <mustafa444.ibrahim555@gmail.com>
2025-04-30 15:08:11 +0300

    create&Insert database

--- lib/main.dart
+++ lib/main.dart
@@ -13,13 +13,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
      debugShowCheckedModeBanner: false,
       home: LayoutScreen(),
-      
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: primaryColor),
         appBarTheme: AppBarTheme(
